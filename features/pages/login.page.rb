6d9840dd043e22ece68afa806010251c54bee0ae
FilippeHarnack <filippeharnack@gmail.com>
2025-03-20 20:29:35 -0300

    add element login-page.rb

--- features/pages/login.page.rb
+++ features/pages/login.page.rb
@@ -1,3 +1,7 @@
 class LoginPage < SitePrism::Page
-  
+
+  element :emailField, :id,"session_key"
+  element :passwordField, :id,"session_password"
+  element :loginButton, :xpath,"//button[@class='sign-in-form__submit-button']"
+
 end
