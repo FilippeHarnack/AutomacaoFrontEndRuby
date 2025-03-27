81160cce6ddb6f5f3b9850ee299893402c5cc43c
FilippeHarnack <filippeharnack@gmail.com>
2025-03-27 14:18:37 -0300

    home-page-usuario

--- features/step_definitions/login.step.rb
+++ features/step_definitions/login.step.rb
@@ -4,8 +4,8 @@ Dado('que o usuario acessa a pagina de login') do
 end
 
 Quando('ele digitar as credenciais validas') do
-  find('input[name="session_key"]', visible: true).set('emersonharnack@gmail.com')  # Preenchendo o e-mail
-  find('input[name="session_password"]', visible: true).set('Life3348')  # Preenchendo a senha (corrigido o name)
+  find('input[name="session_key"]', visible: true).set('automacaoteste141@gmail.com') 
+  find('input[name="session_password"]', visible: true).set('life4276') 
   click_button 'Entrar'
 end
 
